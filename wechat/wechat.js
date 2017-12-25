63b84746dbafdd5194d40e10eff8d3cbe8da6b26
gmw-zjw <gaomingwei2017@163.com>
2017-12-25 14:49:34 +0800

    添加微信消息回复逻辑

--- wechat/wechat.js
+++ wechat/wechat.js
@@ -10,7 +10,6 @@ const WeChat = function (config) {
     this.config = config;
     this.token = config.token;
 }
-module.exports = WeChat;
 
 
 var WeChat = function (config) {
@@ -32,26 +31,26 @@ var WeChat = function (config) {
  * @param {*} url 
  */
 
-this.requestGet = function (url) {
+    this.requestGet = function (url) {
 
-    return new Promise((resolve, reject) => {
-        https.get(url, (res) => {
-            var buffer = [],
-                result = '';
-            // 监听data事件
-            res.on('data', (data) => {
-                buffer.push(data);
-            })
+        return new Promise((resolve, reject) => {
+            https.get(url, (res) => {
+                var buffer = [],
+                    result = '';
+                // 监听data事件
+                res.on('data', (data) => {
+                    buffer.push(data);
+                })
 
-            res.on('end', () => {
-                result = Buffer.concat(buffer, buffer.length).toString('utf-8');
-                resolve(result); // 返回最终结果
+                res.on('end', () => {
+                    result = Buffer.concat(buffer, buffer.length).toString('utf-8');
+                    resolve(result); // 返回最终结果
+                })
+            }).on('error', (err) => {
+                reject(err);
             })
-        }).on('error', (err) => {
-            reject(err);
         })
-    })
-}
+    }
 }
 
 
@@ -110,7 +109,7 @@ WeChat.prototype.getAccessToken = function () {
                 
             })
         } else {
-            
+
             resolve(accessTokenJson.access_token);
         }
 
@@ -118,3 +117,79 @@ WeChat.prototype.getAccessToken = function () {
 
 
 }
+
+/**
+ * 微信消息处理
+ */
+
+ WeChat.prototype.handleMsg = (req, res) => {
+
+    var buffer = [], that = this;
+
+    // 实例化微信消息加密对象
+    var cryptoGraphy = new cryptoGraphy(that.config, req);
+
+    // 监听 data 事件 用于接收数据
+    req.on('data', (data) => {
+        buffer.push(data);
+    });
+
+    // 监听 end 事件 用于处理接收完成的数据
+    req.on('end', () => {
+
+        var msgXml = Buffer.concat(buffer).toString('utf-8');
+
+        // 解析 xml
+        parseString(msgXml, {explicitArray: false}, (err, result) => {
+            if (!err) {
+                result = result.xml;
+                //判断消息加密方式
+                if (req.query.encrypt_type == 'aes') {
+                    result = cryptoGraphy.decryptMsg(result.Encrypt);
+                }
+
+                var toUser = result.ToUserName; //接收微信
+                var feomUser = result.FromUserName; // 发送微信
+                var reportMsg = ''; // 生命回复消息变量
+
+                if (result.MsgType.toLowerCase() === 'event') {
+                    switch(result.Event.toLowerCase()) {
+                        case 'subscribe': 
+                            // 回复消息
+                            var content = '欢迎关注';
+                                content += '1.你是谁 \n';
+                                contnet += '2.关于web \n';
+                                content += '回复文章, 即可获得新推送的文章';
+                            reportMsg = msg.txtMsg(fromUser, toUser, content);
+                        break;
+
+                        case 'click':  
+                            // 自定义菜单
+                            var contnetArr = [
+                                {Title:"Node.js 微信自定义菜单",Description:"使用Node.js实现自定义微信菜单",PicUrl:"http://img.blog.csdn.net/20170605162832842?watermark/2/text/aHR0cDovL2Jsb2cuY3Nkbi5uZXQvaHZrQ29kZXI=/font/5a6L5L2T/fontsize/400/fill/I0JBQkFCMA==/dissolve/70/gravity/SouthEast",Url:"http://blog.csdn.net/hvkcoder/article/details/72868520"},
+                                {Title:"Node.js access_token的获取、存储及更新",Description:"Node.js access_token的获取、存储及更新",PicUrl:"http://img.blog.csdn.net/20170528151333883?watermark/2/text/aHR0cDovL2Jsb2cuY3Nkbi5uZXQvaHZrQ29kZXI=/font/5a6L5L2T/fontsize/400/fill/I0JBQkFCMA==/dissolve/70/gravity/SouthEast",Url:"http://blog.csdn.net/hvkcoder/article/details/72783631"},
+                                {Title:"Node.js 接入微信公众平台开发",Description:"Node.js 接入微信公众平台开发",PicUrl:"http://img.blog.csdn.net/20170605162832842?watermark/2/text/aHR0cDovL2Jsb2cuY3Nkbi5uZXQvaHZrQ29kZXI=/font/5a6L5L2T/fontsize/400/fill/I0JBQkFCMA==/dissolve/70/gravity/SouthEast",Url:"http://blog.csdn.net/hvkcoder/article/details/72765279"}
+                            ];
+                            // 回复图文消息
+                            reportMsg = msg.graphicMsg(fromUser, toUser, contentArr);
+                        break;
+                        default: 
+                            reportMsg = msg.txtMsg(fromUser, toUser, '没有这个选项');
+                        break;    
+                    }
+                }
+                // 判断消息加密方式, 如果未加密则使用明文, 对明文消息进行加密
+                reportMsg = req.query.encrypt_type === 'ase' ? cryptoGraphy.encryptMsg(reportMsg) : reportMsg;  
+                // 返回给微信服务器
+                res.send(reportMsg);
+            } else {
+                // 打印错误
+                console.log(err);
+            }
+        })
+
+    });
+
+ }
+
+ module.exports = WeChat;
